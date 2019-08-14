1413715c1b0f9e4728e6a5d33a633324e71146f3
darioulin <dariomuse95@gmail.com>
2019-08-13 21:00:11 -0500

    Dashboard

--- src/Containers/Dashboard/Dashboard.js
+++ src/Containers/Dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import Navbar from '../../components/navbar/Navbar';
 
 export class Dashboard extends Component {
     render() {
         return (
             <div>
+                <Navbar/>
                 <h1>Desde Dashboard</h1>
             </div>
         )
