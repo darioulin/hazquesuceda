78e6e85dd977cd190490f8e44237d5f3fd67c076
darioulin <dariomuse95@gmail.com>
2019-08-13 22:48:23 -0500

    login route

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Dashboard from './Containers/Dashboard/Dashboard';
 import Landing from './Containers/Landing/Landing';
+import Login from './Containers/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Route exact path="/" component={Landing}/>
+        <Route exact path="/login" component={Login}/>
         <Route exact path="/dashboard" component={Dashboard}/>
       </BrowserRouter>
     </div>
---
+++ src/Containers/Login/Login.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Navbar from '../../components/navbar/Navbar';
+
+export class Login extends Component {
+    render() {
+        return (
+            <div>
+            <Navbar/>
+                <h1>Desde Login</h1>
+            </div>
+        )
+    }
+}
+
+export default Login
